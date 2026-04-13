f5fcec81974b828f92fe020d569efa2a2e09cfd7
salmaarayyan <salmaarayyan@gmail.com>
2026-04-13 09:51:03 +0700

    menambahkan UserController untuk login dan form

--- src/main/java/com/deploy/praktikum6/controller/UserController.java
+++ src/main/java/com/deploy/praktikum6/controller/UserController.java
@@ -1,4 +1,47 @@
 package com.deploy.praktikum6.controller;
 
+import com.deploy.praktikum6.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class UserController {
+    private User userTemp;
+
+    @GetMapping("/")
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username,
+                        @RequestParam String password) {
+        if(username.equals("admin") && password.equals("20230140082")) {
+            return "redirect:/home";
+        }
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("user", userTemp);
+        return "home";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model) {
+        model.addAttribute("user", new User());
+        return "form";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute User user) {
+        userTemp = user;
+        return "redirect:/home";
+    }
+
 }
