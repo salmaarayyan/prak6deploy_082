bbc2a59ee045cabd4193f4093619952fff83b7a4
salmaarayyan <salmaarayyan@gmail.com>
2026-04-13 09:49:28 +0700

    menambahkan model User

--- src/main/java/com/deploy/praktikum6/model/User.java
+++ src/main/java/com/deploy/praktikum6/model/User.java
@@ -1,4 +1,13 @@
 package com.deploy.praktikum6.model;
 
 public class User {
+    private String nama;
+    private String nim;
+
+    // getter setter
+    public String getNama() { return nama; }
+    public void setNama(String nama) { this.nama = nama; }
+
+    public String getNim() { return nim; }
+    public void setNim(String nim) { this.nim = nim; }
 }
